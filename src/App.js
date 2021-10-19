aad989acf6e8b5c827e06e23c3e80f9c7d048b97
Renan Azevedo <renancardan@hotmail.com>
2021-10-19 16:19:23 -0300

    projeto envio 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+
+
+
+function App() {
+    const [InstCad, setInstCad] = useState("Polícia Militar");
+    const [EstadoCad, setEstadoCad] = useState("Maranhão");
+    const [CidadeCad, setCidadeCad] = useState("Bacabal");
+    const [NomeCad, setNomeCad] = useState("");
+    const [TelCad, setTelCad] = useState("");
+    const [EmailCad, setEmailCad] = useState("");
+    const [ContaCad, setContaCad] = useState("");
+    const [SenhaCad, setSenhaCad] = useState("");
+    const [SenhaComfCad, setSenhaComfCad] = useState("");
+
+    const [User, setUser] = useState("L23252679");
+    const [Conta, setConta] = useState("serv");
+    const [Loading, setLoading] = useState(false);
+    const [Alert, setAlert] = useState(" ");
+    const [AlertTipo, setAlertTipo] = useState(" ");
+    const [Dados, setDados] = useState('');
+    const [Avisando, setAvisando] = useState([]);
+   
+
+           
+           
+                 
+
+
+
+  return (
+   <div>
+     <p>
+       Projeto
+     </p>
+   </div>
+    );
+  }
+  
+  export default App;
