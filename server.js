0abd2130d406aa6f7d2fe6fb29aae8febb8338af
Renan Azevedo <renancardan@hotmail.com>
2021-10-19 19:35:47 -0300

    vai da certo

--- server.js
+++ server.js
@@ -1,113 +1,113 @@
-console.lgo("batata");
 
-// const express = require('express')
-// const { resolve } = require('path')
-// const app = express()
 
-// app.use('/', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+const express = require('express')
+const { resolve } = require('path')
+const app = express()
+
+app.use('/', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
-// app.use('/condicionais', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+      )
+)
+app.use('/condicionais', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/chat', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/chat', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/ocorrencia', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/ocorrencia', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/noticia', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/noticia', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/permissoes', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/permissoes', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/contaserv', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/contaserv', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/config', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/config', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/anuncio', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/anuncio', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
-// app.use('/graficos', 
-//   express.static(
-//       resolve(
-//          __dirname,
-//         './build'
-//       )
+app.use('/graficos', 
+  express.static(
+      resolve(
+         __dirname,
+        './build'
+      )
       
-//       )
-// )
+      )
+)
 
 
 
-// app.listen(process.env.PORT || 3000, (err) =>{
-//     if(err) { return console.log(err)}
+app.listen(process.env.PORT || 3000, (err) =>{
+    if(err) { return console.log(err)}
 
-//     console.log('Tudo funcionando certinho')
-// })
+    console.log('Tudo funcionando certinho')
+})
 
